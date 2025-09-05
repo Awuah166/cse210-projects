98e0ae8777a3de1f8faf741d3396ba3c2082f503
Awuah166 <awuahdennis20@gmail.com>
2025-09-05 08:24:31 +0100

    Practicing my understanding on C# in the sandbox folder

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+
+        // Asking the user for their favourite color
+        Console.Write("What is your favourite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine(color);
     }
 }
