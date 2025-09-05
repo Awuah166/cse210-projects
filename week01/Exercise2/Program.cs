c57e38059195ad4820215e29674b244585acf91b
Awuah166 <awuahdennis20@gmail.com>
2025-09-05 12:04:53 +0100

    Did the Exercise 2 for this week . Its a grade program to determine the grade of the user

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,78 @@
 using System;
+using System.Diagnostics.Tracing;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        // Asking the user for their exams score
+        Console.Write("What is your exams score? ");
+        string examScore = Console.ReadLine();
+        int score = int.Parse(examScore);
+
+        string letter = "";
+
+        // Displaying their grade letter depending on their scores.
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Creating a sign variable to determine if the get a "+" or "-"
+        string sign = "";
+        
+        int scoreGrade = score % 10;
+    
+        if (scoreGrade >= 7)
+        {
+            sign = "+";
+        }
+
+        else if (scoreGrade >= 3)
+        {
+            sign = "-";
+        }
+
+        if (letter == "A" && sign == "+")
+        {
+            sign = "";
+        }
+
+        else if (letter == "F" && sign == "-" || sign == "+")
+        {
+            sign = "";
+        }
+        Console.WriteLine($"You had {letter}{sign} grade");
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the exams");
+        }
+
+        else
+        {
+            Console.WriteLine("Better luck next time");
+        }
     }
 }
