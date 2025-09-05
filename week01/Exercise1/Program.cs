dedffb608b5c5f74194a47e6537edeb2f14d3234
Awuah166 <awuahdennis20@gmail.com>
2025-09-05 08:57:13 +0100

    Finied Week01 assignment by asking the user their first ans last name and diplaying it back at them

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Asking the user for their first name
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+        // Asking the user for their last name
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        // Displaying the users full name
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
