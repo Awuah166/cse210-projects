ea1d76a6c5f99f695f174bb0b591f8f2e75df317
Awuah166 <awuahdennis20@gmail.com>
2025-09-06 11:33:33 +0100

    In the Exercise 3 I made the user enter a series of number till they enter 0 to quit. And displayed the total, average and the large number entered by the user

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        // Asking the user for a list of numbers
+        Console.WriteLine("Enter a list of numbers, Enter 0 when finished: ");
+
+        // Creating a list to store the numbers
+        List<int> numbers = new List<int>();
+        int sum = 0;
+        while (true)
+        {
+            Console.Write("Enter Number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            numbers.Add(number);
+            sum += number;
+            if (number == 0)
+            {
+                break;
+            }
+
+        }
+        // Calculatin the total of the numbers
+        Console.WriteLine($"The total is: {sum}");
+
+        // Calculating the average of the numbers
+        double average = (double)sum / (numbers.Count - 1);
+        Console.WriteLine($"The average is: {average}");
+
+        // Displaying the largest number
+        int largest = numbers.Take(numbers.Count - 1).Max();
+        Console.WriteLine($"The largest nnumbe is: {largest}");
     }
 }
