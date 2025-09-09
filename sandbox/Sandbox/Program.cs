c5c8f8df158231fac3419f1a93e3942e8ebf245f
Awuah166 <awuahdennis20@gmail.com>
2025-09-09 14:56:51 +0100

    Did the exercise in week2 by creating a resume and job by using the principle in Abstraction.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
@@ -10,5 +11,37 @@ class Program
         Console.Write("What is your favourite color? ");
         string color = Console.ReadLine();
         Console.WriteLine(color);
+
     }
+
+    // A code template for the category of things known as Person. The
+    // responsibility of a Person is to hold and display personal information.
+    public class Person
+    {
+        // The C# convention is to start member variables with an underscore _
+        public string _givenName = "Dennis";
+        public string _familyName = "Awuah";
+
+        // A special method, called a constructor that is invoked using the  
+        // new keyword followed by the class name and parentheses.
+        public Person()
+        {
+        }
+
+        // A method that displays the person's full name as used in eastern 
+        // countries or <family name, given name>.
+        public void ShowEasternName()
+        {
+            Console.WriteLine($"{_familyName}, {_givenName}");
+        }
+
+        // A method that displays the person's full name as used in western 
+        // countries or <given name family name>.
+        public void ShowWesternName()
+        {
+            Console.WriteLine($"{_givenName} {_familyName}");
+        }
+    }
+
+    
 }
