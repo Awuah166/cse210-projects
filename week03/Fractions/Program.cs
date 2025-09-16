2c4ffb152947872f76633ecdeb65f921c80e6209
Awuah166 <awuahdennis20@gmail.com>
2025-09-16 11:49:50 +0100

    Created a fraction class by practising the principle of encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fractions f1 = new Fractions();      // 1/1
+        Fractions f2 = new Fractions(6);    // 6/1
+        Fractions f3 = new Fractions(6, 7); // 3/4
+
+        // Using setters to change the values and getters to display them
+        f1.SetTop(1);
+        f1.SetBottom(1);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        f2.SetTop(5);
+        f2.SetBottom(1);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        f3.SetTop(3);
+        f3.SetBottom(4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+
     }
 }
